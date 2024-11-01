a5ee1640e06719f50a3d09e1ed613f58c34105f4
SamiraSantos30 <samira30112004@gmail.com>
2024-11-01 18:52:43 -0300

    Update indexAdm.blade.php

--- resources/views/layout/indexAdm.blade.php
+++ resources/views/layout/indexAdm.blade.php
@@ -46,21 +46,13 @@
   <!--Start sidebar-wrapper-->
    <div id="sidebar-wrapper" data-simplebar="" data-simplebar-auto-hide="true">
      <div class="brand-logo">
-<<<<<<< HEAD
       <a href="index.html">
-=======
-      <a href={{ route('pacientes.index') }}>
->>>>>>> 3b20195 (Clinica)
        <img src="/images/logo.png" class="logo-icon" alt="logo icon">
        <h5 class="logo-text">Clinica  </h5>
      </a>
    </div>
    <ul class="sidebar-menu do-nicescrol"> 
-<<<<<<< HEAD
       <li class="sidebar-header">MAIN NAVIGATION</li>
-=======
-      <li class="sidebar-header">ADMINISTRACAO E CONTOLE</li>
->>>>>>> 3b20195 (Clinica)
       <li>
         <a href={{ route('pacientes.index') }}>
           <i class="zmdi zmdi-view-dashboard"></i> <span>Pacientes</span>
@@ -73,44 +65,16 @@
       </li>
 
       <li>
-<<<<<<< HEAD
-        <a href="icons.html">
-          <i class="zmdi zmdi-invert-colors"></i> <span>UI Icons</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="tables.html">
-          <i class="zmdi zmdi-grid"></i> <span>Tables</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="calendar.html">
+        <a href={{ route('agendamentos') }}>
           <i class="zmdi zmdi-calendar-check"></i> <span>Calendar</span>
-=======
-        <a href={{ route('agendamentos')}}>
-          <i class="zmdi zmdi-calendar-check"></i> <span>Calendario</span>
->>>>>>> 3b20195 (Clinica)
           <small class="badge float-right badge-light">New</small>
         </a>
       </li>
 
-<<<<<<< HEAD
-      <li>
-        <a href="profile.html">
-          <i class="zmdi zmdi-face"></i> <span>Profile</span>
-        </a>
-      </li>
-
-      <li>
-        <a href="login.html" target="_blank">
-          <i class="zmdi zmdi-lock"></i> <span>Login</span>
-        </a>
-      </li>
+    
 
        <li>
-        <a href="register.html" target="_blank">
+        <a href={{ route('registrar') }}>
           <i class="zmdi zmdi-account-circle"></i> <span>Registration</span>
         </a>
       </li>
@@ -119,15 +83,6 @@
       <li><a href="javaScript:void();"><i class="zmdi zmdi-coffee text-danger"></i> <span>Important</span></a></li>
       <li><a href="javaScript:void();"><i class="zmdi zmdi-chart-donut text-success"></i> <span>Warning</span></a></li>
       <li><a href="javaScript:void();"><i class="zmdi zmdi-share text-info"></i> <span>Information</span></a></li>
-=======
-       <li>
-        <a href={{ route('registrar')}}>
-          <i class="zmdi zmdi-account-circle"></i> <span>Registre um novo ADM</span>
-        </a>
-      </li>
-
-
->>>>>>> 3b20195 (Clinica)
 
     </ul>
    
@@ -144,15 +99,10 @@
      </a>
     </li>
     <li class="nav-item">
-<<<<<<< HEAD
       <form class="search-bar">
         <input type="text" class="form-control" placeholder="Enter keywords">
          <a href="javascript:void();"><i class="icon-magnifier"></i></a>
       </form>
-=======
-
-    
->>>>>>> 3b20195 (Clinica)
     </li>
   </ul>
      
@@ -175,23 +125,14 @@
         </ul>
     </li>
     <li class="nav-item">
-<<<<<<< HEAD
       <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="#">
-        <span class="user-profile"><img src="https://via.placeholder.com/110x110" class="img-circle" alt="user avatar"></span>
-=======
-      <a class="nav-link dropdown-toggle dropdown-toggle-nocaret" data-toggle="dropdown" href="">
-        <span class="user-profile"><img src="/images/logo.png" class="img-circle" alt="user avatar"></span>
->>>>>>> 3b20195 (Clinica)
+        <span class="user-profile"><img src="images/logo.png" class="img-circle" alt="user avatar"></span>
       </a>
       <ul class="dropdown-menu dropdown-menu-right">
        <li class="dropdown-item user-details">
         <a href="javaScript:void();">
            <div class="media">
-<<<<<<< HEAD
-             <div class="avatar"><img class="align-self-start mr-3" src="https://via.placeholder.com/110x110" alt="user avatar"></div>
-=======
-             <div class="avatar"><img class="align-self-start mr-3" src="/images/logo.png" alt="user avatar"></div>
->>>>>>> 3b20195 (Clinica)
+             <div class="avatar"><img class="align-self-start mr-3" src="images/logo.png" alt="user avatar"></div>
             <div class="media-body">
             <h6 class="mt-2 user-title">Clinica</h6>
             <p class="user-subtitle">clinica@example.com</p>
