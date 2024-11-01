f4f83c665f8997f8a15c7e225fd574ce566baa45
SamiraSantos30 <samira30112004@gmail.com>
2024-11-01 18:53:00 -0300

    Update about.blade.php

--- resources/views/telas/about.blade.php
+++ resources/views/telas/about.blade.php
@@ -6,26 +6,18 @@
 
               <div class="col-md-6 col-sm-6">
                    <div class="about-info">
-<<<<<<< HEAD
                         <h2 class="wow fadeInUp" data-wow-delay="0.6s">Bem-vinda <i class=""></i>a Nossa Clinica</h2>
                         <div class="wow fadeInUp" data-wow-delay="0.8s">
                              <p> Onde oferecemos cuidado integral para a saúde da mulher, unindo ginecologia e nutrição para promover bem-estar e qualidade de vida em todas as fases.</p>
                          
                         </div>
                         <figure class="profile wow fadeInUp" data-wow-delay="1s">
-                             <img src="images/author-image.jpg" class="img-responsive" alt="">
+                             <img src="images/logo.png" class="img-responsive" alt="">
                              <figcaption>
                                   <h3>Dr. Neil Jackson</h3>
                                   <p>General Principal</p>
                              </figcaption>
                         </figure>
-=======
-                        <h2 class="wow fadeInUp" data-wow-delay="0.6s">A NOSSA <i class=""></i>CLINICA FOI</h2>
-                        <div class="wow fadeInUp11" data-wow-delay="0.8s">
-                             <i> Fundada em 1999, nossa clínica nasceu com o propósito de oferecer um atendimento especializado e humanizado nas áreas de ginecologia e nutrição. Ao longo dos anos, crescemos e evoluímos para atender melhor às necessidades de nossas pacientes. Hoje, estamos em um espaço maior e mais estruturado, equipado com o que há de mais moderno para garantir conforto e qualidade nos atendimentos. Com uma equipe altamente qualificada, somos a melhor escolha para cuidar da sua saúde, tanto do presente quanto do futuro, promovendo o equilíbrio entre o corpo e a mente.</i>
-                        </div>
-                       
->>>>>>> 3b20195 (Clinica)
                    </div>
               </div>
               
