b2d150897adbdd603de9ed4517cad7420c2a4bc3
SamiraSantos30 <samira30112004@gmail.com>
2024-11-01 18:54:43 -0300

    Update doctors.blade.php

--- resources/views/telas/doctors.blade.php
+++ resources/views/telas/doctors.blade.php
@@ -1,16 +1,10 @@
 @extends('layout.index')
-<<<<<<< HEAD
 @section('doctors')
 
 <section id="team" data-stellar-background-ratio="1">
     <div class="container">
          <div class="row">
 
-              <div class="col-md-6 col-sm-6">
-                   <div class="about-info">
-                        <h2 class="wow fadeInUp" data-wow-delay="0.1s">Opções Disponiveis:</h2>
-                   </div>
-              </div>
 
               <div class="clearfix"></div>
 
@@ -18,7 +12,7 @@
 
               <div class="col-md-4 col-sm-6">
                    <div class="team-thumb wow fadeInUp" data-wow-delay="0.4s">
-                        <img src="images/team-image2.jpg" class="img-responsive" alt="">
+                        <img src="images/news-image2.jpg" class="img-responsive" alt="">
 
                              <div class="team-info">
                                   <h3>Nutricionista</h3>
@@ -34,25 +28,12 @@
                                   </ul>
                              </div>
 
-=======
-@section('news')
-
-<section id="news" data-stellar-background-ratio="2.5">
-    <div class="container">
-         <div class="wor">
-
-              <div class="col-md-12 col-sm-12">
-                   <!-- SECTION TITLE -->
-                   <div class="section-title wow fadeInUp" data-wow-delay="0.1s">
-                        <h8>ATENDIMENTOS DISPONIVEIS:</h8>
->>>>>>> 3b20195 (Clinica)
                    </div>
               </div>
 
               <div class="col-md-4 col-sm-6">
-<<<<<<< HEAD
                    <div class="team-thumb wow fadeInUp" data-wow-delay="0.6s">
-                        <img src="images/team-image3.jpg" class="img-responsive" alt="">
+                        <img src="images/news-image1.jpg" class="img-responsive" alt="">
 
                              <div class="team-info">
                                   <h3>Ginecologista</h3>
@@ -70,35 +51,6 @@
                    </div>
               </div>
               
-=======
-                   <!-- NEWS THUMB -->
-                   <div class="news-thumb wow fadeInUp" data-wow-delay="0.4s">
-                        <a>
-                             <img src="images/news-image1.jpg" class="img-responsive" alt="">
-                        </a>
-                        <div class="news-info">
-                             <span>💙💙</span>
-                             <h3><a>GINECOLOGISTA</a></h3>
-                             <p>Cuidando da saúde feminina em todas as fases da vida, com atendimento especializado e um olhar atento ao bem-estar integral da mulher.</p>
-                            
-                        </div>
-                   </div>
-              </div>
-
-              <div class="col-md-4 col-sm-6">
-                   <!-- NEWS THUMB -->
-                   <div class="news-thumb wow fadeInUp" data-wow-delay="0.6s">
-                        <a>
-                             <img src="images/news-image2.jpg" class="img-responsive" alt="">
-                        </a>
-                        <div class="news-info">
-                             <span> 💙💙 </span>
-                             <h3><a>NUTRICIONISTA</a></h3>
-                             <p>Promovendo saúde e bem-estar através de uma nutrição equilibrada, personalizada e focada em transformar hábitos para uma vida mais saudável.</p>
-                        </div>
-                   </div>
-              </div>
->>>>>>> 3b20195 (Clinica)
          </div>
     </div>
 </section>
