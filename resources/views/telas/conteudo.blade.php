9628019c31115824f58173213b61aead7204fb2e
SamiraSantos30 <samira30112004@gmail.com>
2024-11-01 18:53:59 -0300

    Update conteudo.blade.php

--- resources/views/telas/conteudo.blade.php
+++ resources/views/telas/conteudo.blade.php
@@ -8,15 +8,9 @@
                         <div class="item item-first">
                              <div class="caption">
                                   <div class="col-md-offset-1 col-md-10">
-<<<<<<< HEAD
-                                       <h3>Let's make your life happier</h3>
-                                       <h1>Healthy Living</h1>
-                                       <a href="#team" class="section-btn btn btn-default smoothScroll">Conheça nossos médica</a>
-=======
-                                     
-                                       <h1>SEJA BEM VINDA!!!</h1>
-                                       <h3>A NOSSA CLINICA!!!</h3>
->>>>>>> 3b20195 (Clinica)
+                                       <h3>Clinica</h3>
+                                       <h1>SEJA BEM VINDA</h1>
+                                      
                                   </div>
                              </div>
                         </div>
@@ -24,16 +18,9 @@
                         <div class="item item-second">
                              <div class="caption">
                                   <div class="col-md-offset-1 col-md-10">
-<<<<<<< HEAD
-                                       <h3>Aenean luctus lobortis tellus</h3>
-                                       <h1>New Lifestyle</h1>
-                                       <a href="#about" class="section-btn btn btn-default btn-gray smoothScroll">More About Us</a>
-=======
-                                      
-                                   <h1>SEJA BEM VINDA!!!</h1>
-                                   <h3>A NOSSA CLINICA!!!</h3>
-                                   
->>>>>>> 3b20195 (Clinica)
+                                   <h3>Clinica</h3>
+                                   <h1>SEJA BEM VINDA</h1>
+                                       
                                   </div>
                              </div>
                         </div>
@@ -41,16 +28,9 @@
                         <div class="item item-third">
                              <div class="caption">
                                   <div class="col-md-offset-1 col-md-10">
-<<<<<<< HEAD
-                                       <h3>Pellentesque nec libero nisi</h3>
-                                       <h1>Your Health Benefits</h1>
-                                       <a href="#news" class="section-btn btn btn-default btn-blue smoothScroll">Read Stories</a>
-=======
-                                      
-                                   <h1>SEJA BEM VINDA!!!</h1>
-                                   <h3>A NOSSA CLINICA!!!</h3>
-                                       
->>>>>>> 3b20195 (Clinica)
+                                   <h3>Clinica</h3>
+                                   <h1>SEJA BEM VINDA</h1>
+                                   
                                   </div>
                              </div>
                         </div>
